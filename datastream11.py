44c337b044400981897e14329c8846e0714ab464
szouridakis <snzouridakis@gmail.com>
2020-07-18 22:34:35 +0300

    Python Code

---
+++ datastream11.py
@@ -0,0 +1,236 @@
+import serial
+import time
+from datetime import datetime
+import h5py
+import numpy as np
+
+BEGIN = b'B'
+STOP = b'S'
+
+
+class Datastream:
+
+    def __init__(self):
+        self.SerialData = serial.Serial('com7', 9600)
+
+        self.Matrix = [[0.0 for x in range(8)] for y in range(8)]  # create a Matrix full of zeros
+        self.x = 0  # initialize variables to zero
+        self.y = 0
+        self.READING_TABLE = False
+        self.READING_COMPLETED = False
+
+        self.MCP1_temp = 0.0
+        self.MCP2_temp = 0.0
+        self.DHT_hum = 0.0
+        self.DHT_temp = 0.0
+        self.DHT_hi = 0.0
+        self.MLXAmb = 0.0
+        self.MLXObj = 0.0
+        self.Moist = 0
+
+        self.Thermal_table_dict = {}
+        self.MCP1_temp_dict = {}
+        self.MCP2_temp_dict = {}
+        self.DHT_hum_dict = {}
+        self.DHT_temp_dict = {}
+        self.DHT_hi_dict = {}
+        self.MLXAmb_dict = {}
+        self.MLXObj_dict = {}
+        self.Moist_dict = {}
+
+        # self.createhdfs()
+
+    def readData(self):
+        if (self.SerialData.inWaiting() > 0):  # if there are data sent from arduino
+            myData = self.SerialData.readline()  # read the first line
+
+            if self.READING_TABLE == True and b']' not in myData:  # if we have started reading the matrix sent from arduino
+                numbers_list = myData.split(b',')  # split the line using the ',' as a seperator
+                for i in range(8):  # fill the row of the matrix
+                    self.Matrix[self.y][self.x] = float(numbers_list[i])  # save in the Matrix the data transformed in floats
+                    self.x = self.x + 1  # go to the next column
+                self.y = self.y + 1  # go to the next row
+                self.x = 0  # go to the first column again
+
+            if b'[' in myData:  # if in the current line there is the '[' means that a new image is sent
+                self.READING_TABLE = True  # in that case we start to read
+                numbers = myData.split(b'[')  # split the data using the '[' as seperator
+                numbers_list = numbers[1].split(b',')  # split them again using the ',' as seperator
+                for i in range(8):  # fill the row of the matrix
+                    self.Matrix[self.y][self.x] = float(
+                        numbers_list[i])  # save in the Matrix the data transformed in floats
+                    self.x = self.x + 1  # go to the next column
+                self.y = self.y + 1  # go to the next row
+                self.x = 0  # go to the first column again
+
+            elif b']' in myData:  # if in the current line there is the ']' means that we have finished reading data
+                self.READING_TABLE = False  # finished reading table
+                self.x = 0  # re-initializing the variables
+                self.y = 0
+                self.Thermal_table_dict[datetime.now().strftime('%Y-%m-%d-%H:%M:%S')] = self.Matrix
+                self.READING_COMPLETED = True
+
+            elif b'x18' in myData:
+                temp_list = myData.split(b':')
+                self.MCP1_temp = float(temp_list[1])
+                self.MCP1_temp_dict[datetime.now().strftime('%Y-%m-%d-%H:%M:%S')] = self.MCP1_temp
+
+            elif b'x19' in myData:
+                temp_list = myData.split(b':')
+                self.MCP2_temp = float(temp_list[1])
+                self.MCP2_temp_dict[datetime.now().strftime('%Y-%m-%d-%H:%M:%S')] = self.MCP2_temp
+
+            elif b'DHTHum' in myData:
+                temp_list = myData.split(b':')
+                self.DHT_hum = float(temp_list[1])
+                self.DHT_hum_dict[datetime.now().strftime('%Y-%m-%d-%H:%M:%S')] = self.DHT_hum
+
+            elif b'DHTTemp' in myData:
+                temp_list = myData.split(b':')
+                self.DHT_temp = float(temp_list[1])
+                self.DHT_temp_dict[datetime.now().strftime('%Y-%m-%d-%H:%M:%S')] = self.DHT_temp
+
+            elif b'DHTHI' in myData:
+                temp_list = myData.split(b':')
+                self.DHT_hi = float(temp_list[1])
+                self.DHT_hi_dict[datetime.now().strftime('%Y-%m-%d-%H:%M:%S')] = self.DHT_hi
+
+            elif b'MLXAmb' in myData:
+                temp_list = myData.split(b':')
+                self.MLXAmb = float(temp_list[1])
+                self.MLXAmb_dict[datetime.now().strftime('%Y-%m-%d-%H:%M:%S')] = self.MLXAmb
+
+            elif b'MLXObj' in myData:
+                temp_list = myData.split(b':')
+                self.MLXObj = float(temp_list[1])
+                self.MLXObj_dict[datetime.now().strftime('%Y-%m-%d-%H:%M:%S')] = self.MLXObj
+
+            elif b'Moisture' in myData:
+                temp_list = myData.split(b':')
+                self.Moist = int(temp_list[1])
+                self.Moist_dict[datetime.now().strftime('%Y-%m-%d-%H:%M:%S')] = self.Moist
+
+    def SerialBegin(self):
+        self.SerialData.write(BEGIN)
+
+    def SerialStop(self):
+        self.SerialData.write(STOP)
+        self.READING_TABLE = False
+        self.Matrix = [[0.0 for x in range(8)] for y in range(8)]
+        self.MCP1_temp = 0.0
+        self.MCP2_temp = 0.0
+        self.DHT_hum = 0.0
+        self.DHT_temp = 0.0
+        self.DHT_hi = 0.0
+        self.MLXAmb = 0.0
+        self.MLXObj = 0.0
+        self.Moist = 0
+
+    def showmeasurements(self):
+
+        print("--------------------------------------------------------")
+        print(self.Matrix)
+        print("MCP1 Temperature is:" + " " + str(self.MCP1_temp) + " °C")
+        print("MCP2 Temperature is:" + " " + str(self.MCP2_temp) + " °C")
+        print("Humidity is:" + " " + str(self.DHT_hum) + " %")
+        print("DHT Temperature is:" + " " + str(self.DHT_temp) + " °C")
+        print("DHT Heat Index is:" + " " + str(self.DHT_hi) + " °C")
+        print("MLX Ambient Temp is:" + " " + str(self.MLXAmb) + " °C")
+        print("MLX Object Temp is:" + " " + str(self.MLXObj) + " °C")
+        print("Moisture Level is:" + " " + str(self.Moist))
+
+    def printdictvalues(self):
+
+        print("-------------")
+        print(self.MCP1_temp_dict)
+        print(self.MCP2_temp_dict)
+        print(self.DHT_hum_dict)
+        print(self.DHT_temp_dict)
+        print(self.DHT_hi_dict)
+        print(self.MLXAmb_dict)
+        print(self.MLXObj_dict)
+        print(self.Moist_dict)
+        print(self.Thermal_table_dict)
+
+    def createhdfs(self):
+
+        timestamp = time.strftime("%Y%m%d-%H%M%S")
+        self.h1 = h5py.File("Thermal-" + timestamp + ".hdf5", 'w')
+        self.h2 = h5py.File("MCP1-" + timestamp + ".hdf5", 'w')
+        self.h3 = h5py.File("MCP2-" + timestamp + ".hdf5", 'w')
+        self.h4 = h5py.File("DHThum-" + timestamp + ".hdf5", 'w')
+        self.h5 = h5py.File("DHTtemp-" + timestamp + ".hdf5", 'w')
+        self.h6 = h5py.File("DHThi-" + timestamp + ".hdf5", 'w')
+        self.h7 = h5py.File("MLXAmb-" + timestamp + ".hdf5", 'w')
+        self.h8 = h5py.File("MLXObj-" + timestamp + ".hdf5", 'w')
+        self.h9 = h5py.File("Moist-" + timestamp + ".hdf5", 'w')
+
+    def savedata(self):
+
+        for k, v in self.Thermal_table_dict.items():
+            self.h1.create_dataset(k, data=np.array(v))
+        self.h1.close()
+        for k, v in self.MCP1_temp_dict.items():
+            self.h2.create_dataset(k, data=np.array(v, dtype=np.single))
+        self.h2.close()
+        for k, v in self.MCP2_temp_dict.items():
+            self.h3.create_dataset(k, data=np.array(v, dtype=np.single))
+        self.h3.close()
+        for k, v in self.DHT_hum_dict.items():
+            self.h4.create_dataset(k, data=np.array(v, dtype=np.single))
+        self.h4.close()
+        for k, v in self.DHT_temp_dict.items():
+            self.h5.create_dataset(k, data=np.array(v, dtype=np.single))
+        self.h5.close()
+        for k, v in self.DHT_hi_dict.items():
+            self.h6.create_dataset(k, data=np.array(v, dtype=np.single))
+        self.h6.close()
+        for k, v in self.MLXAmb_dict.items():
+            self.h7.create_dataset(k, data=np.array(v, dtype=np.single))
+        self.h7.close()
+        for k, v in self.MLXObj_dict.items():
+            self.h8.create_dataset(k, data=np.array(v, dtype=np.single))
+        self.h8.close()
+        for k, v in self.Moist_dict.items():
+            self.h9.create_dataset(k, data=np.array(v, dtype=np.intc))
+        self.h9.close()
+
+    def cleardict(self):
+
+        self.Thermal_table_dict.clear()
+        self.MCP1_temp_dict.clear()
+        self.MCP2_temp_dict.clear()
+        self.DHT_hum_dict.clear()
+        self.DHT_temp_dict.clear()
+        self.DHT_hi_dict.clear()
+        self.MLXAmb_dict.clear()
+        self.MLXObj_dict.clear()
+        self.Moist_dict.clear()
+
+    def hdfhandler(self):
+        self.savedata()
+        self.cleardict()
+        self.createhdfs()
+
+
+if __name__ == '__main__':
+
+    d = Datastream()
+    time.sleep(0.5)
+    d.SerialBegin()
+    d.createhdfs()
+    startTime = time.time()
+
+    while (1):
+
+        d.readData()
+
+        if d.READING_COMPLETED:
+            #time.sleep(0.5)
+            d.showmeasurements()
+            # d.printdictvalues()
+            d.READING_COMPLETED = False
+
+        if time.time() - startTime > 10 * 60:
+            d.hdfhandler()
+            startTime = time.time()
---
+++ ui.py
@@ -0,0 +1,149 @@
+import sys
+
+import seaborn as sb
+import serial
+from PyQt5 import QtCore, QtWidgets
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+from datastream11 import Datastream
+
+
+class Canvas(FigureCanvas):
+
+    def __init__(self, parent=None, width=7, height=5, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+        FigureCanvas.__init__(self, fig)
+        self.setParent(parent)
+
+
+class MyWindow(QtWidgets.QMainWindow):
+
+    def __init__(self):
+        super(MyWindow, self).__init__()
+
+        self.d = Datastream()
+
+        self.setWindowTitle("Arduino Data Receiver")
+        self.setGeometry(30, 30, 1280, 720)
+
+        self.canvas = Canvas(self, width=7, height=5, dpi=100)
+        # self.setCentralWidget(self.canvas)
+        self.canvas.move(0, 0)
+
+        self.start_button = QtWidgets.QPushButton(self)
+        self.start_button.setText("Start")
+        self.start_button.move(100, 600)
+        # self.start_button.setGeometry(QtCore.QRect(0, 0, 200, 40))
+        self.start_button.clicked.connect(self.startPlot)
+
+        self.stop_button = QtWidgets.QPushButton(self)
+        self.stop_button.setText("Stop")
+        self.stop_button.move(250, 600)
+        # self.stop_button.setGeometry(QtCore.QRect(400, 0, 200, 40))
+        self.stop_button.clicked.connect(self.stopPlot)
+
+        self.read_timer = QtCore.QTimer(self)
+        self.read_timer.timeout.connect(self.readData)
+
+        self.save_timer = QtCore.QTimer(self)
+        self.save_timer.timeout.connect(self.d.hdfhandler)
+
+        self.label_1 = QtWidgets.QLabel(self)
+        self.label_1.setText("MCP1 Temperature is:" + " " + str(self.d.MCP1_temp) + " °C")
+        self.label_1.adjustSize()
+        self.label_1.move(850, 50)
+
+        self.label_2 = QtWidgets.QLabel(self)
+        self.label_2.setText("MCP2 Temperature is:" + " " + str(self.d.MCP2_temp) + " °C")
+        self.label_2.adjustSize()
+        self.label_2.move(850, 100)
+
+        self.label_3 = QtWidgets.QLabel(self)
+        self.label_3.setText("Humidity is:" + " " + str(self.d.DHT_hum) + " %")
+        self.label_3.adjustSize()
+        self.label_3.move(850, 150)
+
+        self.label_4 = QtWidgets.QLabel(self)
+        self.label_4.setText("DHT Temperature is:" + " " + str(self.d.DHT_temp) + " °C")
+        self.label_4.adjustSize()
+        self.label_4.move(850, 200)
+
+        self.label_5 = QtWidgets.QLabel(self)
+        self.label_5.setText("DHT Heat Index is:" + " " + str(self.d.DHT_hi) + " °C")
+        self.label_5.adjustSize()
+        self.label_5.move(850, 250)
+
+        self.label_6 = QtWidgets.QLabel(self)
+        self.label_6.setText("MLX Ambient Temp is:" + " " + str(self.d.MLXAmb) + " °C")
+        self.label_6.adjustSize()
+        self.label_6.move(850, 300)
+
+        self.label_7 = QtWidgets.QLabel(self)
+        self.label_7.setText("MLX Object Temp is:" + " " + str(self.d.MLXObj) + " °C")
+        self.label_7.adjustSize()
+        self.label_7.move(850, 350)
+
+        self.label_8 = QtWidgets.QLabel(self)
+        self.label_8.setText("Moisture Level is:" + " " + str(self.d.Moist))
+        self.label_8.setStyleSheet("background-color: lightgreen; border: 1px solid black;")
+        self.label_8.adjustSize()
+        self.label_8.move(850, 400)
+
+        self.heat_map = sb.heatmap(self.d.Matrix, vmin=20, vmax=45, annot=True, fmt='', ax=self.canvas.axes)
+
+    def plotData(self):
+        self.heat_map.clear()
+        self.heat_map = sb.heatmap(self.d.Matrix, vmin=20, vmax=45, annot=True, fmt='', ax=self.canvas.axes, cbar=False)
+        self.canvas.draw()
+        self.label_1.setText("MCP1 Temperature is:" + " " + str(self.d.MCP1_temp) + " °C")
+        self.label_1.adjustSize()
+        self.label_2.setText("MCP2 Temperature is:" + " " + str(self.d.MCP2_temp) + " °C")
+        self.label_2.adjustSize()
+        self.label_3.setText("Humidity is:" + " " + str(self.d.DHT_hum) + " %")
+        self.label_3.adjustSize()
+        self.label_4.setText("DHT Temperature is:" + " " + str(self.d.DHT_temp) + " °C")
+        self.label_4.adjustSize()
+        self.label_5.setText("DHT Heat Index is:" + " " + str(self.d.DHT_hi) + " °C")
+        self.label_5.adjustSize()
+        self.label_6.setText("MLX Ambient Temp is:" + " " + str(self.d.MLXAmb) + " °C")
+        self.label_6.adjustSize()
+        self.label_7.setText("MLX Object Temp is:" + " " + str(self.d.MLXObj) + " °C")
+        self.label_7.adjustSize()
+        self.label_8.setText("Moisture Level is:" + " " + str(self.d.Moist))
+
+        if self.d.Moist >= 500:
+            self.label_8.setStyleSheet("background-color: orange; border: 1px solid black;")
+        elif self.d.Moist <= 250:
+            self.label_8.setStyleSheet("background-color: lightgreen; border: 1px solid black;")
+        else:
+            self.label_8.setStyleSheet("background-color: yellow; border: 1px solid black;")
+        self.label_8.adjustSize()
+
+    def readData(self):
+        self.d.readData()
+        if self.d.READING_COMPLETED:
+            self.plotData()
+            self.d.READING_COMPLETED = False
+
+    def startPlot(self):
+        self.d.SerialBegin()
+        self.d.createhdfs()
+        self.read_timer.start()
+        self.save_timer.start(10 * 60 * 1000)
+
+    def stopPlot(self):
+        self.read_timer.stop()
+        self.save_timer.stop()
+        self.d.savedata()
+        self.d.cleardict()
+        self.d.SerialStop()
+        self.plotData()
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    win = MyWindow()
+    win.show()
+
+    sys.exit(app.exec_())
