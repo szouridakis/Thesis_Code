4a7e13145d350b78343edca2a8436da5d1cd6432
szouridakis <snzouridakis@gmail.com>
2020-09-26 16:21:03 +0300

    generated new plots

--- data_handler2.py
+++ data_handler2.py
@@ -21,7 +21,7 @@ data6 = []
 
 ############################ extract data from hdf5 files ###############################
 
-for filename in glob.glob('C:/Users/stelz/Documents/Python-last/experiments/exp13/MCP1*.hdf5'):
+for filename in glob.glob('C:/Users/stelz/Google Drive/Thesis_Stelios_Zouridakis/Files/meas_and_plots2/experiments/exp5b/MCP1*.hdf5'):
 ##for filename in glob.glob('C:/Users/Stelios/Desktop/Thesis/experiments/exp1a/MCP1*.hdf5'):
 
     hf = h5py.File(filename, 'r')
@@ -32,7 +32,7 @@ for filename in glob.glob('C:/Users/stelz/Documents/Python-last/experiments/exp1
         data1.append(data_set[()])
     hf.close()
 
-for filename in glob.glob('C:/Users/stelz/Documents/Python-last/experiments/exp13/MLXObj*.hdf5'):
+for filename in glob.glob('C:/Users/stelz/Google Drive/Thesis_Stelios_Zouridakis/Files/meas_and_plots2/experiments/exp5b/MCP2*.hdf5'):
 ##for filename in glob.glob('C:/Users/Stelios/Desktop/Thesis/experiments/exp1a/MLXObj*.hdf5'):
 
     hf = h5py.File(filename, 'r')
@@ -43,29 +43,29 @@ for filename in glob.glob('C:/Users/stelz/Documents/Python-last/experiments/exp1
         data2.append(data_set[()])
     hf.close()
 
-#for filename in glob.glob('C:/Users/stelz/Documents/Python-last/experiments/exp12/MCP1*.hdf5'):
+for filename in glob.glob('C:/Users/stelz/Google Drive/Thesis_Stelios_Zouridakis/Files/meas_and_plots2/experiments/exp5b/MLXObj*.hdf5'):
 #for filename in glob.glob('C:/Users/Stelios/Desktop/Thesis/experiments/exp1b/MCP1*.hdf5'):
-#
-#    hf = h5py.File(filename, 'r')
-#
-#    for group_key in hf.keys():
-#        timestamps3.append(group_key)
-#        data_set = hf[group_key]
-#        data3.append(data_set[()])
-#    hf.close()
 
-#for filename in glob.glob('C:/Users/stelz/Documents/Python-last/experiments/exp12/MCP1*.hdf5'):
+    hf = h5py.File(filename, 'r')
+
+    for group_key in hf.keys():
+        timestamps3.append(group_key)
+        data_set = hf[group_key]
+        data3.append(data_set[()])
+    hf.close()
+
+for filename in glob.glob('C:/Users/stelz/Google Drive/Thesis_Stelios_Zouridakis/Files/meas_and_plots2/experiments/exp5b/MLXAmb*.hdf5'):
 #for filename in glob.glob('C:/Users/Stelios/Desktop/Thesis/experiments/exp1b/MLXObj*.hdf5'):
-#
-#    hf = h5py.File(filename, 'r')
-#
-#    for group_key in hf.keys():
-#        timestamps4.append(group_key)
-#        data_set = hf[group_key]
-#        data4.append(data_set[()])
-#    hf.close()
 
-#for filename in glob.glob('C:/Users/stelz/Documents/Python-last/experiments/exp12/MCP1*.hdf5'):
+    hf = h5py.File(filename, 'r')
+
+    for group_key in hf.keys():
+        timestamps4.append(group_key)
+        data_set = hf[group_key]
+        data4.append(data_set[()])
+    hf.close()
+
+#for filename in glob.glob('C:/Users/stelz/Google Drive/Thesis_Stelios_Zouridakis/Files/meas_and_plots2/experiments/exp12/MCP1*.hdf5'):
 #for filename in glob.glob('C:/Users/Stelios/Desktop/Thesis/experiments/exp1c/MCP1*.hdf5'):
 #
 #    hf = h5py.File(filename, 'r')
@@ -76,7 +76,7 @@ for filename in glob.glob('C:/Users/stelz/Documents/Python-last/experiments/exp1
 #        data5.append(data_set[()])
 #    hf.close()
 
-#for filename in glob.glob('C:/Users/stelz/Documents/Python-last/experiments/exp12/MCP1*.hdf5'):
+#for filename in glob.glob('C:/Users/stelz/Google Drive/Thesis_Stelios_Zouridakis/Files/meas_and_plots2/experiments/exp12/MCP1*.hdf5'):
 #for filename in glob.glob('C:/Users/Stelios/Desktop/Thesis/experiments/exp1c/MLXObj*.hdf5'):
 #
 #    hf = h5py.File(filename, 'r')
@@ -87,7 +87,7 @@ for filename in glob.glob('C:/Users/stelz/Documents/Python-last/experiments/exp1
 #        data6.append(data_set[()])
 #    hf.close()
 
-for filename in glob.glob('C:/Users/stelz/Documents/Python-last/experiments/exp13/Thermal*.hdf5'):
+for filename in glob.glob('C:/Users/stelz/Google Drive/Thesis_Stelios_Zouridakis/Files/meas_and_plots2/experiments/exp5b/Thermal*.hdf5'):
 #for filename in glob.glob('C:/Users/Stelios/Desktop/Thesis/experiments/exp1b/Thermal*.hdf5'):
 
     hf = h5py.File(filename, 'r')
@@ -140,8 +140,8 @@ for filename in glob.glob('C:/Users/stelz/Documents/Python-last/experiments/exp1
 
 data1.pop()
 data2.pop()
-##data3.pop()
-##data4.pop()
+data3.pop()
+data4.pop()
 #data5.pop()
 #data6.pop()
 
@@ -155,8 +155,12 @@ plt.figure()
 #plt.plot(timestamps7, data8, label="80 cm")
 #plt.plot(timestamps7, data9, label="1 m")
 plt.plot(timestamps5, data1, label="MCP1 on Object")
-plt.plot(timestamps5, data2, label="MLX Object")
+plt.plot(timestamps5, data2, label="MCP2 Ambient")
 plt.plot(timestamps5, data5, label="Thermal Camera MAX")
+plt.plot(timestamps5, data6, label="Thermal Camera MIN")
+plt.plot(timestamps5, data3, label="MLX Object")
+plt.plot(timestamps5, data4, label="MLX Ambient")
+
 ##plt.plot(timestamps5, data4, label="MLX 80")
 plt.legend(loc="upper right")
 
@@ -164,12 +168,16 @@ plt.legend(loc="upper right")
 ##plt.xlabel('Time on 2020-07-14 from 18:37:20 till 19:07:19') ##1b remove the first 100 samples because of MLX Calibration
 ##plt.xlabel('Time on 2020-07-14 from 18:39:52 till 19:07:19') ##1b remove the first 100 samples because of MLX Calibration
 ##plt.xlabel('Time on 2020-07-15 from 14:32:47 till 15:02:46') ##1c
-#plt.xlabel('Time on 2020-07-15 from 15:11:37 till 15:31:37') ##2a
+##plt.xlabel('Time on 2020-07-15 from 15:11:37 till 15:31:37') ##2a
 ##plt.xlabel('Time on 2020-07-15 from 15:39:22 till 15:59:21') ##2b
 ##plt.xlabel('Time on 2020-07-15 from 16:23:11 till 16:43:10') ##3a
 ##plt.xlabel('Time on 2020-07-15 from 16:54:04 till 17:14:03') ##3b
-#plt.title('Experiment 1\n\nDifference of Object\'s Temperature Between MCP1 and MLX on Different Cases\n(MCP1 - MLXObj)')
-plt.xlabel('Time')
+##plt.xlabel('Time on 2020-09-17 from 14:12:13 till 14:32:11') ##4a
+##plt.xlabel('Time on 2020-09-17 from 14:46:10 till 15:06:08') ##4b
+##plt.xlabel('Time on 2020-09-17 from 16:12:04 till 16:32:02') ##5a
+plt.xlabel('Time on 2020-09-17 from 15:18:17 till 15:38:17') ##5b
+plt.title('Experiment 5B\n\nChange of Object\'s Temperature Related to Ambient Temperature')
+#plt.xlabel('Time')
 plt.ylabel('Temperature')
 
 
